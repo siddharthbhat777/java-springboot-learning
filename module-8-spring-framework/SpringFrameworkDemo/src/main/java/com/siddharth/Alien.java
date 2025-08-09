78c49910d1b36197bc4cbd215ba34b28a171e49d
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-09 20:31:20 +0530

    dependency injections

--- module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Alien.java
+++ module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Alien.java
@@ -1,9 +1,19 @@
 package com.siddharth;
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
     private int age;
+    private Laptop laptop;
+
     public Alien() {
         System.out.println("Alien object created");
+    }
+
+    @ConstructorProperties({ "age", "laptop" }) // for "name"  attr constructor injection
+    public Alien(int age, Laptop laptop) {
+        this.age = age;
+        this.laptop = laptop;
     }
 
     public int getAge() {
@@ -15,7 +25,16 @@ public class Alien {
         this.age = age;
     }
 
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
     public void code() {
         System.out.println("Coding...");
+        laptop.compile();
     }
 }
--- module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Laptop.java
+++ module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Laptop.java
@@ -4,4 +4,8 @@ public class Laptop {
     public Laptop() {
         System.out.println("Laptop object created");
     }
+
+    public void compile() {
+        System.out.println("Compiling...");
+    }
 }
