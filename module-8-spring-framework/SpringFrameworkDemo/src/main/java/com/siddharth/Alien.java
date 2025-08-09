daabaf7a77236088498ae25846967bda34fdf34b
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-09 16:14:36 +0530

    setter injection

--- module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Alien.java
+++ module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Alien.java
@@ -1,10 +1,18 @@
 package com.siddharth;
 
 public class Alien {
-    int age;
-
+    private int age;
     public Alien() {
         System.out.println("Alien object created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter called");
+        this.age = age;
     }
 
     public void code() {
--- module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/App.java
+++ module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/App.java
@@ -5,19 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // object will be created here
         Alien obj1 = (Alien) context.getBean("alien1"); // we are just using already created object
-        obj1.age = 25;
-        System.out.println(obj1.age);
+        System.out.println(obj1.getAge());
         obj1.code();
-        Alien obj2 = (Alien) context.getBean("alien1"); // as we are just using already created object, so it wont be created twice
+        /*Alien obj2 = (Alien) context.getBean("alien1"); // as we are just using already created object, so it wont be created twice
         System.out.println(obj2.age);
-        obj2.code();
+        obj2.code();*/
     }
 }
