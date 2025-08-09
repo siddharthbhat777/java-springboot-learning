80a608a8fab779bba72dbef99eca62547736d380
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-09 23:00:23 +0530

    using class type and inner bean

--- module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/App.java
+++ module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/App.java
@@ -9,11 +9,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // object will be created here
-        Alien obj1 = (Alien) context.getBean("alien1"); // we are just using already created object
+        Alien obj1 = context.getBean("alien1", Alien.class); // we are just using already created object
         System.out.println(obj1.getAge());
         obj1.code();
         /*Alien obj2 = (Alien) context.getBean("alien1"); // as we are just using already created object, so it wont be created twice
         System.out.println(obj2.age);
         obj2.code();*/
+
+        // Desktop obj = (Desktop) context.getBean("computer2");
+        // If you don't wanna do type casting
+        Desktop obj2 = context.getBean("computer2", Desktop.class);
+        // if there is just one bean of this class
+        Desktop obj3 = context.getBean(Desktop.class);
     }
 }
--- module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Desktop.java
+++ module-8-spring-framework/SpringFrameworkDemo/src/main/java/com/siddharth/Desktop.java
@@ -1,6 +1,11 @@
 package com.siddharth;
 
 public class Desktop implements Computer {
+
+    public Desktop() {
+        System.out.println("Desktop object created");
+    }
+
     @Override
     public void compile() {
         System.out.println("Compiling using Desktop");
