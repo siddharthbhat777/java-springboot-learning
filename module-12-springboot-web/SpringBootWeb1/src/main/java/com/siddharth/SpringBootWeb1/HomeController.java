db96c4d7806558f9917cb9d09b064cf0d40503c6
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-15 02:13:48 +0530

    created jsp page and used it

---
+++ module-12-springboot-web/SpringBootWeb1/src/main/java/com/siddharth/SpringBootWeb1/HomeController.java
@@ -0,0 +1,13 @@
+package com.siddharth.SpringBootWeb1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index.jsp";
+    }
+}
