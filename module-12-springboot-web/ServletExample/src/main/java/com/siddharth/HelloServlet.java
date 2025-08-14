0006675879373e8f21d1089b7e59c1b437ddaa1e
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-14 18:42:55 +0530

    printing output with servlet

--- module-12-springboot-web/ServletExample/src/main/java/com/siddharth/HelloServlet.java
+++ module-12-springboot-web/ServletExample/src/main/java/com/siddharth/HelloServlet.java
@@ -3,10 +3,18 @@ package com.siddharth;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
 
-    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println("In Service");
+
+        resp.setContentType("text/html"); // allows you to print with html
+
+        // resp.getWriter().println("Hello World!");
+        PrintWriter out = resp.getWriter();
+        out.println("<h2>Hello World!</h2>");
     }
 }
