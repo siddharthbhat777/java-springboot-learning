0a6f505384d7154486d4a2f01d577fe3d4556141
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-16 01:41:37 +0530

    spring way

--- module-12-springboot-web/SpringBootWeb1/src/main/java/com/siddharth/SpringBootWeb1/HomeController.java
+++ module-12-springboot-web/SpringBootWeb1/src/main/java/com/siddharth/SpringBootWeb1/HomeController.java
@@ -3,17 +3,20 @@ package com.siddharth.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "index.jsp";
+        return "index";
     }
 
-    @RequestMapping("add")
+    // Servlet and Session way
+    /*@RequestMapping("add")
     public String add(HttpServletRequest req, HttpSession session) {
 
         int num1 = Integer.parseInt(req.getParameter("num1"));
@@ -24,5 +27,16 @@ public class HomeController {
 
         System.out.println(result);
         return "result.jsp";
+    }*/
+
+    // Spring way with param and model object
+    @RequestMapping("add")
+    public String add(int num1, @RequestParam("num2") int number2, Model model) {
+        int result = num1 + number2;
+
+        model.addAttribute("result", result); // passing data to ui
+
+        System.out.println(result);
+        return "result";
     }
 }
