118823719881110d0afe1ec082fef8003c07e30e
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-16 23:51:02 +0530

    worked with layers and added data successfully

--- module-14-building-project/JobApp/src/main/java/com/siddharth/JobApp/JobController.java
+++ module-14-building-project/JobApp/src/main/java/com/siddharth/JobApp/JobController.java
@@ -1,12 +1,17 @@
 package com.siddharth.JobApp;
 
 import com.siddharth.JobApp.model.JobPost;
+import com.siddharth.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobController {
+
+    @Autowired
+    private JobService service;
 
     @GetMapping({"/", "home"})
     public String home() {
@@ -20,6 +25,7 @@ public class JobController {
 
     @PostMapping("handleForm") // function name used in addjob jsp file
     public String handleForm(JobPost jobPost) {
+        service.addJob(jobPost);
         return "success";
     }
 }
---
+++ module-14-building-project/JobApp/src/main/java/com/siddharth/JobApp/service/JobService.java
@@ -0,0 +1,23 @@
+package com.siddharth.JobApp.service;
+
+import com.siddharth.JobApp.model.JobPost;
+import com.siddharth.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs() {
+        return repo.getAllJobs();
+    }
+}
