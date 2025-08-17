0d9d8690e228c958bb2b052292c92c4e78ecd410
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-17 19:43:46 +0530

    connecting to UI. Adding CORS policy.

--- module-15-REST-with-springboot/JOBPORTAL/src/components/Search.jsx
+++ module-15-REST-with-springboot/JOBPORTAL/src/components/Search.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
 import {
-    AppBar,
-    Toolbar,
-    Box,
-    Card,
-    Grid,
-    Typography,
-  } from "@mui/material";
-  import axios from "axios";
-  import { useEffect, useState } from "react";
+  AppBar,
+  Toolbar,
+  Box,
+  Card,
+  Grid,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Search = () => {
-    const [post, setPost] = useState(null);
+  const [post, setPost] = useState(null);
 
-    useEffect(() => {
-        const fetchInitialPosts = async () => {
-            const response = await axios.get(`http://localhost:8000/posts`);
-            console.log(response);
-            setPost(response.data);
-        }
-         fetchInitialPosts();
-      }, []);
+  useEffect(() => {
+    const fetchInitialPosts = async () => {
+      const response = await axios.get(`http://localhost:8080/jobPosts`);
+      console.log(response);
+      setPost(response.data);
+    }
+    fetchInitialPosts();
+  }, []);
 
   return (
-        <Grid container spacing={2} sx={{ margin: "2%" }}>
-                  <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
- 
-          <Typography variant="h6" align='center' component="div" sx={{ flexGrow: 1 }}>
-            Job Portal
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <Grid container spacing={2} sx={{ margin: "2%" }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+
+            <Typography variant="h6" align='center' component="div" sx={{ flexGrow: 1 }}>
+              Job Portal
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Grid item xs={12} sx={12} md={12} lg={12}>
       </Grid>
       {post &&
@@ -45,9 +45,9 @@ const Search = () => {
                   variant="h5"
                   sx={{ fontSize: "2rem", fontWeight: "600" }}
                 >
-             {p.postProfile}
+                  {p.postProfile}
                 </Typography>
-                <Typography sx={{ color: "#585858", marginTop:"2%" }} variant="body" >
+                <Typography sx={{ color: "#585858", marginTop: "2%" }} variant="body" >
                   Description: {p.postDesc}
                 </Typography>
                 <br />
@@ -56,7 +56,7 @@ const Search = () => {
                   Years of Experience: {p.reqExperience} years
                 </Typography>
 
-                <Typography gutterBottom  variant="body">Skills : </Typography>
+                <Typography gutterBottom variant="body">Skills : </Typography>
                 {p.postTechStack.map((s, i) => {
                   return (
                     <Typography variant="body" gutterBottom key={i}>
@@ -65,7 +65,7 @@ const Search = () => {
                     </Typography>
                   );
                 })}
-  
+
               </Card>
             </Grid>
           );
--- module-15-REST-with-springboot/spring-boot-rest/src/main/java/com/siddharth/spring_boot_rest/JobRestController.java
+++ module-15-REST-with-springboot/spring-boot-rest/src/main/java/com/siddharth/spring_boot_rest/JobRestController.java
@@ -4,6 +4,7 @@ import com.siddharth.spring_boot_rest.model.JobPost;
 import com.siddharth.spring_boot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 
     @Autowired
