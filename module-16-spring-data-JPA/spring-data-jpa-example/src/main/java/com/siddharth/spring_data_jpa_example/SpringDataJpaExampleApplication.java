ba93acf04fe0d12eb82db92af128915fe4afb1be
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-18 01:55:55 +0530

    using update and delete

--- module-16-spring-data-JPA/spring-data-jpa-example/src/main/java/com/siddharth/spring_data_jpa_example/SpringDataJpaExampleApplication.java
+++ module-16-spring-data-JPA/spring-data-jpa-example/src/main/java/com/siddharth/spring_data_jpa_example/SpringDataJpaExampleApplication.java
@@ -15,19 +15,19 @@ public class SpringDataJpaExampleApplication {
 
         StudentRepo repo = context.getBean(StudentRepo.class);
 
-        /*Student s1 = context.getBean(Student.class);
+        // Student s1 = context.getBean(Student.class);
         Student s2 = context.getBean(Student.class);
-        Student s3 = context.getBean(Student.class);
+        // Student s3 = context.getBean(Student.class);
 
-        s1.setRollNo(101);
+        /*s1.setRollNo(101);
         s1.setName("Siddharth");
-        s1.setMarks(94);
+        s1.setMarks(94);*/
 
         s2.setRollNo(102);
-        s2.setName("Ruchika");
-        s2.setMarks(86);
+        s2.setName("Ruchika Ganacharya");
+        s2.setMarks(92);
 
-        s3.setRollNo(103);
+        /*s3.setRollNo(103);
         s3.setName("Vasudha");
         s3.setMarks(65);
 
@@ -42,7 +42,13 @@ public class SpringDataJpaExampleApplication {
         /*Optional<Student> student = repo.findById(105);
         System.out.println(student.orElse(new Student()));*/ // returns blank object if data not found
 
-        System.out.println(repo.findByName("Siddharth")); // created custom method
-        System.out.println(repo.findByMarksGreaterThan(72)); // created custom method
+        /*System.out.println(repo.findByName("Siddharth")); // created custom method
+        System.out.println(repo.findByMarksGreaterThan(72)); // created custom method*/
+
+        // update data
+        repo.save(s2);
+
+        // delete data
+        repo.delete(s2);
 	}
 }
