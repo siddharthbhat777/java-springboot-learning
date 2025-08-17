ec2c668ca8d83ed1e724359b262bd486d824d15d
Siddharth Bhat <dbgt4531@gmail.com>
2025-08-17 23:31:54 +0530

    initialized student jdbc project

---
+++ module-16-spring-data-JPA/SpringJDBCExample/src/main/java/com/siddharth/SpringJDBCExample/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.siddharth.SpringJDBCExample.service;
+
+import com.siddharth.SpringJDBCExample.model.Student;
+import com.siddharth.SpringJDBCExample.repo.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private StudentRepo repo;
+
+    public StudentRepo getRepo() {
+        return repo;
+    }
+
+    @Autowired
+    public void setRepo(StudentRepo repo) {
+        this.repo = repo;
+    }
+
+    public void addStudent(Student student) {
+        repo.save(student);
+    }
+
+    public List<Student> getStudents() {
+        return repo.findAll();
+    }
+}
