904f8a36c933dfab5051cc31bf05902d34b8b26e
Siddharth Bhat <dbgt4531@gmail.com>
2025-11-17 23:31:39 +0530

    backend api basic mapping added and added correct frontend project

--- module-17-spring-boot-mvc/project-1/frontend/src/main.jsx
+++ module-17-spring-boot-mvc/project-1/frontend/src/main.jsx
@@ -2,15 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { useContext } from "react";
-import { AppProvider } from "./Context/Context.jsx";
-// import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Router> */}
-      <AppProvider>
-        <App />
-      </AppProvider>
-    {/* </Router> */}
+    <App />
   </React.StrictMode>
 );
