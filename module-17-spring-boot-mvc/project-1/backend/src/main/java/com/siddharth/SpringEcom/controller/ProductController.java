d4d4ffb51d58626ca0a2dbc40e8cdbbca0f8fa85
Siddharth Bhat <dbgt4531@gmail.com>
2025-11-18 23:28:47 +0530

    setting status code

--- module-17-spring-boot-mvc/project-1/backend/src/main/java/com/siddharth/SpringEcom/controller/ProductController.java
+++ module-17-spring-boot-mvc/project-1/backend/src/main/java/com/siddharth/SpringEcom/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.siddharth.SpringEcom.controller;
 import com.siddharth.SpringEcom.model.Product;
 import com.siddharth.SpringEcom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK); // setting status code with ResponseEntity
     }
 }
