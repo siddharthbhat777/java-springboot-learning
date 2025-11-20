dc850148b6af231755620f40476f2ccfbd503085
Siddharth Bhat <dbgt4531@gmail.com>
2025-11-20 23:49:10 +0530

    added new UI and added add product functionality

--- module-17-spring-boot-mvc/project-1/backend/src/main/java/com/siddharth/SpringEcom/model/Product.java
+++ module-17-spring-boot-mvc/project-1/backend/src/main/java/com/siddharth/SpringEcom/model/Product.java
@@ -1,10 +1,7 @@
 package com.siddharth.SpringEcom.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,6 +26,10 @@ public class Product {
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob // large object
+    private byte[] imageData;
 
     public Product(int id) {
         this.id = id;
