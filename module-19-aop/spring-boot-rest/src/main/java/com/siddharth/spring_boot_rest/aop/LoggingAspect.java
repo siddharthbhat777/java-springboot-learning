9fa609f100d86b05e37f976216f26371044a96ec
Siddharth Bhat <dbgt4531@gmail.com>
2025-11-30 00:42:15 +0530

    added new advices

--- module-19-aop/spring-boot-rest/src/main/java/com/siddharth/spring_boot_rest/aop/LoggingAspect.java
+++ module-19-aop/spring-boot-rest/src/main/java/com/siddharth/spring_boot_rest/aop/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.siddharth.spring_boot_rest.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,6 +16,24 @@ public class LoggingAspect {
     // Advise(point_cut)
     @Before("execution(* com.siddharth.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.siddharth.spring_boot_rest.service.JobService.updateJob(..))")
     public void logMethodCall(JoinPoint jp) {
-        LOGGER.info("Method called" + jp.getSignature().getName());
+        LOGGER.info("Method called " + jp.getSignature().getName());
+    }
+
+    // like finally
+    @After("execution(* com.siddharth.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.siddharth.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodExecuted(JoinPoint jp) {
+        LOGGER.info("Method Executed " + jp.getSignature().getName());
+    }
+
+    // like catch
+    @AfterThrowing("execution(* com.siddharth.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.siddharth.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodCrash(JoinPoint jp) {
+        LOGGER.info("Method has some issues " + jp.getSignature().getName());
+    }
+
+    // if success
+    @AfterReturning("execution(* com.siddharth.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.siddharth.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint jp) {
+        LOGGER.info("Method Executed Successfully " + jp.getSignature().getName());
     }
 }
