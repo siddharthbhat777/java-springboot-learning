61667f5f80e527f8026eed1dc8dd3a3b2e60aa66
Siddharth Bhat <dbgt4531@gmail.com>
2025-11-30 00:58:48 +0530

    monitoring using around advice

---
+++ module-19-aop/spring-boot-rest/src/main/java/com/siddharth/spring_boot_rest/aop/PerformanceMonitorAspect.java
@@ -0,0 +1,28 @@
+package com.siddharth.spring_boot_rest.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerformanceMonitorAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PerformanceMonitorAspect.class);
+
+    @Around("execution(* com.siddharth.spring_boot_rest.service.JobService.*(..))")
+    public Object monitorTime(ProceedingJoinPoint jp) throws Throwable { // ProceedingJoinPoint means ongoing function execution
+        long start = System.currentTimeMillis();
+
+        Object obj = jp.proceed();
+
+        long end = System.currentTimeMillis();
+
+        LOGGER.info("Time taken: " + jp.getSignature().getName() + " : " + (end - start) + " ms");
+
+        return obj;
+    }
+}
