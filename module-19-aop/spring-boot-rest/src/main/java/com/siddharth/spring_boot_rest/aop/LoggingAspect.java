1cb207a59d4e8a3634211dcd3be52903d9861a05
Siddharth Bhat <dbgt4531@gmail.com>
2025-11-29 23:57:37 +0530

    using aspect with aspect type

--- module-19-aop/spring-boot-rest/src/main/java/com/siddharth/spring_boot_rest/aop/LoggingAspect.java
+++ module-19-aop/spring-boot-rest/src/main/java/com/siddharth/spring_boot_rest/aop/LoggingAspect.java
@@ -1,12 +1,19 @@
 package com.siddharth.spring_boot_rest.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
+    // execution(return_type class_name_with_package.method_name(arguments))
+    @Before("execution(* com.siddharth.spring_boot_rest.service.JobService.*(..))")
     public void logMethodCall() {
         LOGGER.info("Method called");
     }
