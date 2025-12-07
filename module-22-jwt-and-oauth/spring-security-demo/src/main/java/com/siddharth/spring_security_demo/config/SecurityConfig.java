2faf3f4bc34160c4961be10f41a0e8f8a97f5a6b
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-07 22:55:25 +0530

    login functionality added

--- module-22-jwt-and-oauth/spring-security-demo/src/main/java/com/siddharth/spring_security_demo/config/SecurityConfig.java
+++ module-22-jwt-and-oauth/spring-security-demo/src/main/java/com/siddharth/spring_security_demo/config/SecurityConfig.java
@@ -3,14 +3,17 @@ package com.siddharth.spring_security_demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
-        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return provider;
     }
 
@@ -32,11 +35,16 @@ public class SecurityConfig {
         http
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/register")
+                        .requestMatchers("/register", "/login")
                         .permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
     }
 }
--- module-22-jwt-and-oauth/spring-security-demo/src/main/java/com/siddharth/spring_security_demo/controller/UserController.java
+++ module-22-jwt-and-oauth/spring-security-demo/src/main/java/com/siddharth/spring_security_demo/controller/UserController.java
@@ -3,7 +3,9 @@ package com.siddharth.spring_security_demo.controller;
 import com.siddharth.spring_security_demo.model.User;
 import com.siddharth.spring_security_demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+
     @Autowired
     private UserService service;
-    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    // private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     @PostMapping("register")
     public User register(@RequestBody User user) {
-        user.setPassword(encoder.encode(user.getPassword()));
+        // user.setPassword(encoder.encode(user.getPassword()));
         return service.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if (authentication.isAuthenticated())
+            return "Success";
+        else
+            return "Login Failed";
     }
 }
