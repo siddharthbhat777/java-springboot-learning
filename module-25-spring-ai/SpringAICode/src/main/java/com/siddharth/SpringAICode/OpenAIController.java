43acb5bbf3de68d48b974701d8d7d351014bf9cd
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-14 22:48:09 +0530

    using OpenAI LLM model

---
+++ module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/OpenAIController.java
@@ -0,0 +1,22 @@
+package com.siddharth.SpringAICode;
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenAIController {
+
+    private OpenAiChatModel chatModel;
+
+    public OpenAIController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/api/{message}")
+    public String getAnswer(@PathVariable String message) {
+        String response = chatModel.call(message);
+        return response;
+    }
+}
