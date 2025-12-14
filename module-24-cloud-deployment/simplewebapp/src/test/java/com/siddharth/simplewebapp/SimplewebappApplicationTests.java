f1b17a28f37a5f24f5bf0d541e3f472536b26e2c
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-14 18:52:25 +0530

    created spring app for deployment

---
+++ module-24-cloud-deployment/simplewebapp/src/test/java/com/siddharth/simplewebapp/SimplewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharth.simplewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
