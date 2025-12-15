53ba7c16680230d87cc6a7f8958948c2dfa58b05
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-15 23:02:18 +0530

    using ollama mistral on local

--- module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/OllamaController.java
+++ module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/OllamaController.java
@@ -5,41 +5,27 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ChatClientController {
+public class OllamaController {
 
     private ChatClient chatClient;
     private ChatMemory chatMemory;
 
-    // Using ChatClient programmatically if you have multiple different models
-    public ChatClientController(OpenAiChatModel chatModel) {
+    public OllamaController(OllamaChatModel chatModel) {
         this.chatClient = ChatClient.create(chatModel);
     }
 
-    // Using ChatClient autoConfigured Builder if you have single model
-    /*public ChatClientController(ChatClient.Builder builder) {
-        this.chatMemory = MessageWindowChatMemory.builder().build();
-        this.chatClient = builder
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
-                .build();
-    }*/
-
-    @GetMapping("/api/openai/chat/{message}")
+    @GetMapping("/api/ollama/chat/{message}")
     public ResponseEntity<String> getAnswer(@PathVariable String message) {
-        // String response = chatClient.prompt(message).call().content(); // getting LLM model response directly
-
-        // More options using ChatResponse
         ChatResponse chatResponse = chatClient.prompt(message).call().chatResponse();
-
         System.out.println(chatResponse.getMetadata().getModel());
-
-        String response = chatResponse.getResult().getOutput().getText(); // same response but with different way with more options
+        String response = chatResponse.getResult().getOutput().getText();
         return ResponseEntity.ok(response);
     }
 }
