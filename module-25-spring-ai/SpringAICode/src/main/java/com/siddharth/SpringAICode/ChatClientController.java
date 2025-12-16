beb0800525b8706d59a58bb97fc1ee9efb11108a
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-16 19:24:16 +0530

    structuring prompt using PromptTemplate

--- module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/ChatClientController.java
+++ module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/ChatClientController.java
@@ -5,11 +5,13 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class ChatClientController {
@@ -41,5 +43,31 @@ public class ChatClientController {
 
         String response = chatResponse.getResult().getOutput().getText(); // same response but with different way with more options
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/recommend")
+    public String recommend(@RequestParam String type, @RequestParam String year, @RequestParam String lang) {
+        String tempt = """
+                I want to watch a {type} movie tonight with good rating,
+                looking for movies around this year {year}.
+                The language I am looking for is {lang}.
+                Suggest one specific movie and tell me the cast and length of the movie.
+                
+                response format should be:
+                1. Movie Name
+                2. basic plot
+                3. cast
+                4. length
+                5. IMDB rating
+                """;
+        PromptTemplate promptTemplate = new PromptTemplate(tempt);
+        Prompt prompt = promptTemplate.create(Map.of("type", type, "year", year, "lang", lang));
+
+        String response = chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+
+        return response;
     }
 }
