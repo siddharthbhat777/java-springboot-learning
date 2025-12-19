9bd898c5c463880acbb0a0a50f514826885b4dd4
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-19 19:42:30 +0530

    using PgVector with docker compose

--- module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/AppConfig.java
+++ module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/AppConfig.java
@@ -3,15 +3,21 @@ package com.siddharth.SpringAICode;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public VectorStore vectorStore(@Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
-        return SimpleVectorStore.builder(embeddingModel).build();
+    public VectorStore vectorStore(JdbcTemplate jdbcTemplate, @Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                .dimensions(1536)
+                .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)
+                .indexType(PgVectorStore.PgIndexType.HNSW)
+                .build();
     }
 }
