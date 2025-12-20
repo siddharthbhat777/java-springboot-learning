a09a331aeb5b58df087215bd6aad92914d9f4b25
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-20 22:46:43 +0530

    configuring Audio Transcriptions

--- module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/AudioGenController.java
+++ module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/AudioGenController.java
@@ -1,6 +1,9 @@
 package com.siddharth.SpringAICode;
 
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,12 @@ public class AudioGenController {
 
     @PostMapping("/api/stt")
     public String speechToText(@RequestParam MultipartFile file) {
-        return audioModel.call(file.getResource());
+        OpenAiAudioTranscriptionOptions options = OpenAiAudioTranscriptionOptions
+                .builder()
+                .language("es")
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.SRT)
+                .build();
+        AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(file.getResource(), options);
+        return audioModel.call(prompt).getResult().getOutput();
     }
 }
