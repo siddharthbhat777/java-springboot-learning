361eee307d1da77d5a5ceadee85826a7d40a76a0
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-20 23:23:19 +0530

    list output converter

---
+++ module-25-spring-ai/SpringAICode/src/main/java/com/siddharth/SpringAICode/MovieController.java
@@ -0,0 +1,32 @@
+package com.siddharth.SpringAICode;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+    private ChatClient chatClient;
+
+    public MovieController(OpenAiChatModel chatModel) {
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+    @GetMapping("movies")
+    public List<String> getMovies(@RequestParam String name) {
+        List<String> movies = chatClient.prompt()
+                .user(u -> u.text("List top 5 movies of {name}").param("name", name))
+                .call()
+                .entity(new ListOutputConverter(new DefaultConversionService()));
+
+
+        return movies;
+
+    }
+}
