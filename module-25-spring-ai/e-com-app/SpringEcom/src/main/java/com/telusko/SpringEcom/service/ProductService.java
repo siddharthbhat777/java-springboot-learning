fb74979cafc07500ea3ab4f5db33506bfe4ce73d
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-23 21:45:18 +0530

    generating description with chatClient

--- module-25-spring-ai/e-com-app/SpringEcom/src/main/java/com/telusko/SpringEcom/service/ProductService.java
+++ module-25-spring-ai/e-com-app/SpringEcom/src/main/java/com/telusko/SpringEcom/service/ProductService.java
@@ -2,6 +2,7 @@ package com.telusko.SpringEcom.service;
 
 import com.telusko.SpringEcom.model.Product;
 import com.telusko.SpringEcom.repo.ProductRepo;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepo productRepo;
+
+    @Autowired
+    private ChatClient chatClient;
 
     public List<Product> getAllProducts() {
         return productRepo.findAll();
@@ -45,6 +49,20 @@ public class ProductService {
     }
 
     public String generateDescription(String name, String category) {
-        return "Demo description";
+        String descPrompt = String.format("""
+                
+                Write a concise and professional product description for an e-commerce listing.
+                
+                Product Name: %s
+                Category: %s
+
+                Keep it simple, engaging, and highlight its primary features or benefits.
+                Avoid technical jargon and keep it customer-friendly.
+                Limit the description to 250 characters maximum.
+                
+                """, name, category);
+
+        String desc = chatClient.prompt(descPrompt).call().chatResponse().getResult().getOutput().getText();
+        return desc;
     }
 }
