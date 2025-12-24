3c9879aae15ac89138b287c53dc32f00d7e4c37c
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-24 14:47:09 +0530

    add question feature added

--- module-26-microservices/quizapp/src/main/java/com/siddharth/quizapp/controller/QuestionController.java
+++ module-26-microservices/quizapp/src/main/java/com/siddharth/quizapp/controller/QuestionController.java
@@ -3,10 +3,7 @@ package com.siddharth.quizapp.controller;
 import com.siddharth.quizapp.model.Question;
 import com.siddharth.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class QuestionController {
     @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 }
